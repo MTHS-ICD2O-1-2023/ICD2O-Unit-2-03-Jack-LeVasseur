0e3e47d3c674c0a26c4b1e1bac184f864ccc66e9
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-03-19 18:26:19 +0000

    Updated index.html Updated syle.css Updated street.jpg Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jack LeVasseur
+// Created on: March 18,2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users Street name and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("first-name").value
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your adress is: " +440+ ",Longfilds" + 440 + "."
+}
